f3e0999036b60e2e426765c28c25faf4e85afc6e
fidanyusifova <yusifovafidan09@gmail.com>
2024-01-03 21:02:44 +0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from "./pages/HomePage.jsx"
+import CardsPage from './pages/CardsPage.jsx';
+import ContactPage from './pages/ContactPage.jsx';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/contactpage" element={<ContactPage />} />
+          <Route path="/cards" element={<CardsPage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
